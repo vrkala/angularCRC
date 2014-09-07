3d96ef8ee0354e5c09d600add7383c5fda3df2ea
Adrian Ng <wu.89@ufl.edu>
2014-09-07 16:56:22 -0400

    finish the rest
    wait for adjust

--- js/app.js
+++ js/app.js
@@ -42,4 +42,35 @@ angular.module('myApp', ['ngRoute'])
 	
 		// use the HTML5 History API
 		$locationProvider.html5Mode(true);
-	});
+	})
+   .service('PageTitle', function() {
+      var title = 'Productmate';
+      return {
+        title: function() { return title; },
+        setTitle: function(newTitle) { title = newTitle; }
+      };
+    })
+.service('MetaInformation', function() {
+      var metaDescription = '';
+      var metaKeywords = '';
+      return {
+        metaDescription: function() { return metaDescription; },
+        metaKeywords: function() { return metaKeywords; },
+        reset: function() {
+          metaDescription = '';
+          metaKeywords = '';
+        },
+        setMetaDescription: function(newMetaDescription) {
+          metaDescription = newMetaDescription;
+        },
+        appendMetaKeywords: function(newKeywords) {
+          for (var key in newKeywords) {
+            if (metaKeywords === '') {
+              metaKeywords += newKeywords[key].name;
+            } else {
+              metaKeywords += ', ' + newKeywords[key].name;
+            }
+          }
+        }
+      };
+    });
