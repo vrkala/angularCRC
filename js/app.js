1324b74fbe6b2cd0f987c5e2dce586d428477c4c
Adrian Ng <wu.89@ufl.edu>
2014-09-07 16:07:06 -0400

    save before
    switch

--- js/app.js
+++ js/app.js
@@ -1,43 +1,41 @@
-var myApp = angular.module('myApp', [
-  'ngRoute'
-]);
-
-myApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-  when('/student', {
-    templateUrl: 'partials/student.html',
-    controller: 'ListController'
-  }).
-   when('/e2', {
-    templateUrl: 'partials/e2.html',
-    controller: 'ListController'
-  }).
-  when('/employer', {
-    templateUrl: 'partials/employer.html',
-    controller: 'employerCtrl'
-  }).
-  when('/faculty', {
-    templateUrl: 'partials/faculty.html',
-    controller: 'ListController'
-  }).
-   when('/alumni', {
-    templateUrl: 'partials/alumni.html',
-    controller: 'ListController'
-  }).
-   when('/about', {
-    templateUrl: 'partials/about.html',
-    controller: 'ListController'
-  }).
-   when('/contact', {
-    templateUrl: 'partials/contact.html',
-    controller: 'ListController'
-  }).
-  when('/employer/gatorcareerlink', {
-    templateUrl: 'partials/gatorcareerlink.html',
-    controller: 'ListController'
-  }).
-  otherwise({
-    redirectTo: '/',
-    templateUrl: 'partials/student.html'
+angular.module('myApp', ['ngRoute', 'ngResource']
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+		  .when('/student', {
+		templateUrl: 'partials/student.html',
+		controller: 'ListController'
+	  }).
+	   when('/e2', {
+		templateUrl: 'partials/e2.html',
+		controller: 'ListController'
+	  }).
+	  when('/employer', {
+		templateUrl: 'partials/employer.html',
+		controller: 'employerCtrl'
+	  }).
+	  when('/faculty', {
+		templateUrl: 'partials/faculty.html',
+		controller: 'ListController'
+	  }).
+	   when('/alumni', {
+		templateUrl: 'partials/alumni.html',
+		controller: 'ListController'
+	  }).
+	   when('/about', {
+		templateUrl: 'partials/about.html',
+		controller: 'ListController'
+	  }).
+	   when('/contact', {
+		templateUrl: 'partials/contact.html',
+		controller: 'ListController'
+	  }).
+	  when('/employer/gatorcareerlink', {
+		templateUrl: 'partials/gatorcareerlink.html',
+		controller: 'ListController'
+	  }).
+	  otherwise({
+		redirectTo: '/',
+		templateUrl: 'partials/student.html'
+	  });
+    $locationProvider.html5Mode(true);
   });
-}]);
